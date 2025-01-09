ad7de7df175d945391a1b5781ae6209df30bdf82
Jack Walker <jwalkerack7@gmail.com>
2025-01-09 17:20:18 +0000

    Update error with premier league mapping

--- reference.py
+++ reference.py
@@ -16,7 +16,7 @@ leagues = {
     "Scottish Championship": "https://www.bbc.co.uk/sport/football/scottish-championship/scores-fixtures",
     "Scottish League One": "https://www.bbc.co.uk/sport/football/scottish-league-one/scores-fixtures",
     "Scottish League Two": "https://www.bbc.co.uk/sport/football/scottish-league-two/scores-fixtures",
-    "English Premiership": "https://www.bbc.co.uk/sport/football/scottish-championship/scores-fixtures",
+    "English Premiership": "https://www.bbc.co.uk/sport/football/premier-league/scores-fixtures",
     "English Championship": "https://www.bbc.co.uk/sport/football/championship/scores-fixtures",
     "English League One": "https://www.bbc.co.uk/sport/football/league-one/scores-fixtures",
     "English League Two": "https://www.bbc.co.uk/sport/football/league-two/scores-fixtures"
